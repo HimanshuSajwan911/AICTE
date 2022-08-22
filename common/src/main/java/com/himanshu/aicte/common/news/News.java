298686c53c90e58976071fee64b7579a6c96fc8d
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2022-08-22 21:15:33 +0530

    POJO class for news object.

---
+++ common/src/main/java/com/himanshu/aicte/common/news/News.java
@@ -0,0 +1,52 @@
+package com.himanshu.aicte.common.news;
+
+
+import com.google.firebase.Timestamp;
+
+public class News {
+
+    private String headline, body, author;
+    private Timestamp timestamp;
+
+    public News() {
+    }
+
+    public News(String headline, String body, String author, Timestamp timestamp) {
+        this.headline = headline;
+        this.body = body;
+        this.author = author;
+        this.timestamp = timestamp;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
