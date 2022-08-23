9b14d59a4d9b24f37e600af4f1fd972e7b9a0b93
HimanshuSajwan911 <himanshusajwan911@gmail.com>
2022-08-23 18:26:54 +0530

    splash screen activity.

---
+++ aicteadmin/src/main/java/com/himanshu/aicteadmin/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package com.himanshu.aicteadmin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SystemClock.sleep(300);
+
+        Intent intentMain = new Intent(SplashScreenActivity.this, MainActivity.class);
+
+        startActivity(intentMain);
+        finish();
+    }
+}
